c102755ede6d9e2c6d6276be14cc8fddfa328806
arthurhoreanu <arthur.horeanu@gmail.com>
2025-02-18 11:01:56 +0200

    2b

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.*;
+import model.Character;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Objects;
 public class Controller {
 
     private List<Product> products = new ArrayList<>();
+    private List<Character> characters = new ArrayList<>();
 
     // CRUD Product
 
@@ -50,6 +52,71 @@ public class Controller {
      */
     public void deleteProduct(String name) {
         products.removeIf(product -> Objects.equals(product.getName(), name));
+    }
+
+    // CRUD Character
+
+    /**
+     * Adds character to the InMemory repository.
+     * @param character
+     */
+    public void addCharacter(model.Character character) {
+        for( Character characterFromList : characters) {
+            if(characterFromList.getId() == character.getId()) {
+                throw new IllegalArgumentException("Character already exists");
+            }
+        }
+        characters.add(character);
+    }
+
+    /**
+     * Gets all characters from the InMemory repository.
+     * @return
+     */
+    public List<model.Character> getCharacterList() {
+        return characters;
+    }
+
+    /**
+     * Updates characters from the InMemory repository.
+     * @param id
+     * @param newName
+     * @param newRegion
+     */
+    public void updateCharacter(int id, String newName, String newRegion) {
+        for (model.Character character : characters) {
+            if (character.getId() == id) {
+                character.setName(newName);
+                character.setRegion(newRegion);
+            }
+        }
+    }
+
+    /**
+     * Deletes characters from the InMemory repository.
+     * @param id
+     */
+    public void deleteCharacter(int id) {
+        characters.removeIf(character -> character.getId() == id);
+    }
+
+    // Helper method: buy
+
+    /**
+     * Helper method in order for characters to buy products.
+     * @param characterID
+     * @param productName
+     */
+    public void buyProducts(int characterID, String productName) {
+        for (model.Character character : characters) {
+            if (character.getId() == characterID) {
+                for (Product product : products) {
+                    if (Objects.equals(product.getName(), productName)) {
+                        character.getProducts().add(product);
+                    }
+                }
+            }
+        }
     }
 
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,6 @@
 package view;
 
+import model.Character;
 import model.Product;
 import controller.*;
 
@@ -18,7 +19,16 @@ public class View {
             System.out.println("2. Show All Products");
             System.out.println("3. Update Product");
             System.out.println("4. Delete Product");
+            System.out.println("");
 
+            // Second object
+            System.out.println("5. Add Character");
+            System.out.println("6. Show All Characters");
+            System.out.println("7. Update Character");
+            System.out.println("8. Delete Character");
+
+            // Helper „Buy” method
+            System.out.println("9. Buy Product");
 
             int choice = scanner.nextInt();
             scanner.nextLine(); // Consume newline
@@ -28,6 +38,12 @@ public class View {
                 case 2 -> showAllProducts();
                 case 3 -> updateProduct();
                 case 4 -> deleteProduct();
+
+                case 5 -> addCharacter();
+                case 6 -> showAllCharacters();
+                case 7 -> updateCharacter();
+                case 8 -> deleteCharacter();
+                case 9 -> buyProducts();
             }
         }
     }
@@ -80,4 +96,66 @@ public class View {
         String name = scanner.nextLine();
         controller.deleteProduct(name);
     }
+
+    // CRUD Character
+
+    /**
+     * Adds character to the InMemory repository.
+     */
+    private void addCharacter() {
+        System.out.println("Enter the character ID");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the character name");
+        String name = scanner.nextLine();
+        System.out.println("Enter the character region");
+        String region = scanner.nextLine();
+        controller.addCharacter(new Character(id, name, region));
+    }
+
+    /**
+     * Gets all characters from the InMemory repository.
+     */
+    private void showAllCharacters() {
+        controller.getCharacterList().forEach(System.out::println);
+    }
+
+    /**
+     * Updates character from the InMemory repository.
+     */
+    private void updateCharacter() {
+        controller.getCharacterList().forEach(System.out::println);
+        System.out.println("Enter the character ID");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the character name");
+        String name = scanner.nextLine();
+        System.out.println("Enter the character region");
+        String region = scanner.nextLine();
+        controller.updateCharacter(id, name, region);
+    }
+
+    /**
+     * Deletes character from the InMemory repository.
+     */
+    private void deleteCharacter() {
+        System.out.println("Enter the character ID");
+        int id = scanner.nextInt();
+        controller.deleteCharacter(id);
+    }
+
+    /**
+     * Helper method: characters buy products.
+     */
+    private void buyProducts() {
+        showAllCharacters();
+        showAllProducts();
+        System.out.println("Enter the character ID");
+        int characterID = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the product name");
+        String productName = scanner.nextLine();
+        controller.buyProducts(characterID, productName);
+    }
+
 }
